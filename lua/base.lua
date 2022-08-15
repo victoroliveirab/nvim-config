ab68e4c31dc345cf2ebb6427f3e8fafa2de8235a
Victor Oliveira <eu@victoroliveira.com.br>
2022-08-15 15:42:04 -0300

    adds lualine

--- lua/base.lua
+++ lua/base.lua
@@ -34,7 +34,6 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 
 -- Try these later
 -- vim.opt.inccommand = 'split'
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,6 +48,9 @@ return packer.startup(function(use)
   -- Colorscheme
   use "folke/tokyonight.nvim"
 
+  -- Statusline
+  use "nvim-lualine/lualine.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
