b4cc1fbe97667d2f0f75e4f26c71c928b06e891d
Victor Oliveira <eu@victoroliveira.com.br>
2022-08-18 21:40:01 -0300

    telescope fzf

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 ## dependencies
 - `tree-sitter-cli` -> `cargo install tree-sitter-cli` or `npm i -g tree-sitter-cli`
+- `ripgrep` -> `sudo apt-get install ripgrep`
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -76,6 +76,9 @@ return packer.startup(function(use)
   use "windwp/nvim-autopairs"
   use "windwp/nvim-ts-autotag"
 
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-telescope/telescope-file-browser.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
