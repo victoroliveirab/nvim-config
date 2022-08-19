58944295babfe2829bbbcbcd8baca5a02e9c9d78
Victor Oliveira <eu@victoroliveira.com.br>
2022-08-18 21:00:41 -0300

    autopairs and autotag

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -73,6 +73,9 @@ return packer.startup(function(use)
     run = ':TSUpdate'
   }
 
+  use "windwp/nvim-autopairs"
+  use "windwp/nvim-ts-autotag"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
